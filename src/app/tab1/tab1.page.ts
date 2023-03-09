ccade735f7a38ae927c67be591a4fe4325d060fe
giuliabferreira <giuliabferreira@hotmail.com>
2023-03-09 12:29:55 -0300

    .

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,10 +1,20 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  animations: [
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotateY(0)' })),
+      state('rotated', style({ transform: 'rotateY(-360deg)' })),
+      transition('rotated => default', animate('2000ms ease-out')),
+      transition('default => rotated', animate('2000ms ease-in'))
+    ])
+  ]
 })
+
 export class Tab1Page {
 
   cara = 'assets/cara.png';
@@ -12,17 +22,27 @@ export class Tab1Page {
   logo = 'assets/logo.png';
   image = this.logo;
   info = 'Clique no botão para jogar!';
+  state = 'default';
 
   constructor() {}
 
   jogarMoeda(){
-    if(Math.random() < 0.5){
-      this.image = this.cara;
-      this.info = "Cara!"
-    }else{
-      this.image = this.coroa;
-      this.info = "Coroa!"
-    }
+    this.info = 'Girando...';
+    this.image = this.logo;
+    this.state = this.state === 'default' ? 'rotated' : 'default';
+
+    setTimeout(() => {
+
+      if(Math.random() < 0.5){
+        this.image = this.cara;
+        this.info = "Cara!"
+      } else {
+        this.image = this.coroa;
+        this.info = "Coroa!"
+      }
+
+    }, 2000);
+
   }
 
 }
